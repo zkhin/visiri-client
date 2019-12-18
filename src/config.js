acc90a897a3a2d0a35f551bdc38f076990b621ea
zkhin <arandomname@gmail.com>
2019-12-17 20:30:41 -0500

    deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://infinite-brushlands-69470.herokuapp.com/api',
+  API_ENDPOINT: 'https://infinite-brushlands-69470.herokuapp.com/',
   API_KEY: process.env.REACT_APP_API_KEY,
-  TOKEN_KEY: 'auth-token',
+  TOKEN_KEY: 'fejijowieojgfiajweo',
 }
