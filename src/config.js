47e92c4f02dee2d8b23ca55a141b86894f818e8a
zkhin <arandomname@gmail.com>
2019-12-17 20:25:24 -0500

    deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://infinite-brushlands-69470.herokuapp.com/api',
+  API_KEY: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: 'auth-token',
 }
