a77229fa1d23188d4e7a7438369ab89d252559af
zkhin <arandomname@gmail.com>
2019-12-18 01:20:49 -0500

    updates

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -170,7 +170,6 @@ export default class Upload extends Component {
         const deltaYBounded = !(wheelEvent.deltaY % 1) ?
           Math.abs(Math.min(-100, Math.max(100, wheelEvent.deltaY))) :
           Math.abs(wheelEvent.deltaY)
-        // const scaleBy = .99 + deltaYBounded / 99
         const scaleBy = .99 + deltaYBounded / 1000
         const newScale = wheelEvent.deltaY > 0 ? oldScale / scaleBy : oldScale * scaleBy
         stage.scale({ x: newScale, y: newScale })
@@ -205,7 +204,7 @@ export default class Upload extends Component {
 
   dblClickTap = (stage) => {
     if (stage) {
-      stage.addEventListener('dblclick', (evt) => {
+      stage.addEventListener('dblclick dbltap', (evt) => {
         if (!this.state.touching) {
           this.createCellRegion(evt)
         }
