a51904e1c632406c91d2a5e30cc4aed7e4ee240a
zkhin <arandomname@gmail.com>
2019-12-17 19:46:18 -0500

    deployable

--- server/src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://zayar@localhost/visiri',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-}
