885cf7a78be731b4e795979148964b7d22dc2f9b
zkhin <arandomname@gmail.com>
2020-02-01 22:47:18 -0500

    landing page changes and demo

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,33 +1,54 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import './Landing.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Landing.css";
 
 export default class Landing extends Component {
   render() {
     return (
       <>
-        <section>
+        <section className="Landing">
           <img className="splash" src="/Completed 2.jpg" alt="Visiri"></img>
-          <Link to="/register" className="menu">Register</Link>
-          <Link to="/login" className="menu">Login</Link>
-          <Link className="menu" to="/upload">Demo</Link>
+          <Link to="/register" className="menu">
+            Register
+          </Link>
+          <Link to="/login" className="menu">
+            Login
+          </Link>
+          <Link className="menu" to="/upload">
+            Demo
+          </Link>
+          <div className="about">
+            <p>
+              Visiri is an application that allows users to upload cell
+              microscopy images and create datasets that will allow deep
+              learning models to automatically count cells.
+            </p>
+            <p>
+              Using a mobile device camera linked to the eyepiece of a
+              microscope, a neural network must be trained with user's
+              laboratory-specific imaging equipment and cells in order for
+              automated counting to work.
+            </p>
+            <p>
+              In this current build, users can upload images of their cells and
+              label the cells in a mobile-friendly browser-based interface. It
+              supports touch events such as pinch zooming and dragging as well
+              as desktop mouse/wheel events.
+            </p>
+          </div>
           <ul className="intro">
+            <li>Take a picture of the cells you want to count</li>
             <li>
-              Take a picture of the cells you want to count.
+              Mark the image with square boxes containing an individual cell
             </li>
+            <li>Click on your cell in the toolbox to review its position</li>
             <li>
-              Mark the image with square boxes containing an individual cell.
-            </li>
-            <li>
-              Click on your cell in the toolbox to change its position or delete it.
-            </li>
-            <li>
-              When you are done, click Finished to save and upload the data to your collection of experiments.
+              When you are done, click Finished to save and upload the data to
+              your collection of experiments
             </li>
           </ul>
-
         </section>
       </>
-    )
+    );
   }
 }
--- src/routes/LoginPage.js
+++ src/routes/LoginPage.js
@@ -1,29 +1,35 @@
-import React, { Component } from 'react'
-import LoginForm from '../components/LoginForm/LoginForm'
-import { Section } from '../components/Utils/Utils'
+import React, { Component } from "react";
+import LoginForm from "../components/LoginForm/LoginForm";
+import { Section } from "../components/Utils/Utils";
+import "./LoginPage.css";
 
 export default class LoginPage extends Component {
   static defaultProps = {
     location: {},
     history: {
-      push: () => {},
-    },
-  }
+      push: () => {}
+    }
+  };
 
   handleLoginSuccess = () => {
-    const { history } = this.props
-    const destination = '/experiments'
-    history.push(destination)
-  }
+    const { history } = this.props;
+    const destination = "/experiments";
+    history.push(destination);
+  };
 
   render() {
     return (
-      <Section className='LoginPage'>
+      <Section className="LoginPage">
         <h2>Login</h2>
-        <LoginForm
-          onLoginSuccess={this.handleLoginSuccess}
-        />
+        <div className="demo-tip">
+          You may also use the demo account credentials: <br />
+          <code>
+            username: demo <br />
+            password: password
+          </code>
+        </div>
+        <LoginForm onLoginSuccess={this.handleLoginSuccess} />
       </Section>
-    )
+    );
   }
 }
